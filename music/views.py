5ddf952f69c3235e2288964a08f1ccfeea2f24d7
lionellbriones <llenoil@gmail.com>
2016-03-31 21:28:19 +0800

    Added music list

--- music/views.py
+++ music/views.py
@@ -1,13 +1,15 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
 from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 def detail(request, album_id):
-    return HttpResponse("<h1>Details for Album id: " + str(album_id) + "<h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+
+    return render(request, 'music/detail.html', {'album': album})
