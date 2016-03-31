cab209ee9a4cdd2df3a6908d9f5e8ccad880e200
lionellbriones <llenoil@gmail.com>
2016-03-31 19:50:53 +0800

    First Commit

---
+++ music/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from .models import Album
+
+def index(request):
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
+
+def detail(request, album_id):
+    return HttpResponse("<h1>Details for Album id: " + str(album_id) + "<h2>")
