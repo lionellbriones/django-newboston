5cb5f407a0c8dd13a4ff6cb6a82733e28c951e43
lionellbriones <llenoil@gmail.com>
2016-04-01 13:14:18 +0800

    Add Favorite form

--- music/models.py
+++ music/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Album(models.Model):
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
@@ -9,10 +10,12 @@ class Album(models.Model):
     def __str__(self):
         return self.album_title + ' - ' + self.artist
 
+
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+    is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
         return self.song_title
--- music/views.py
+++ music/views.py
@@ -1,15 +1,16 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
+
 
 def index(request):
     all_albums = Album.objects.all()
     return render(request, 'music/index.html', {'all_albums': all_albums})
 
-def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
 
+def detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+    return False
